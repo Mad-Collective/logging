17be8b0bd0112aae45cbee1aaf175013ecf59c1f
Jaroslaw Gabara <jaroslaw.gabara@teamcmp.com>
2016-09-30 11:50:12 +0200

    fixed test handler builder

--- src/Monolog/Handler/TestHandlerBuilder.php
+++ src/Monolog/Handler/TestHandlerBuilder.php
@@ -7,10 +7,18 @@ use Monolog\Handler\TestHandler;
 class TestHandlerBuilder implements HandlerBuilderInterface
 {
     /**
+     * @var TestHandler
+     */
+    private $handler;
+
+    /**
      * @inheritDoc
      */
     public function build($channelName, FormatterInterface $formatter, $processors = [])
     {
-        return new TestHandler();
+        if (is_null($this->handler)) {
+            $this->handler = new TestHandler();
+        }
+        return $this->handler;
     }
 }
