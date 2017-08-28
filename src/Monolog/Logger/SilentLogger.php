10678d80d34b82bbedea19e1c50740ba3642ed7a
joelcorominas <joelcorominas@Joels-iMac-3.local>
2017-08-28 11:11:13 +0200

    Added silent logger again

---
+++ src/Monolog/Logger/SilentLogger.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Cmp\Logging\Monolog\Logger;
+
+use Monolog\Logger;
+
+class SilentLogger extends Logger
+{
+    protected $defaultContext = [];
+
+    /**
+     * Adds a log record.
+     *
+     * @param  integer $level   The logging level
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function addRecord($level, $message, array $context = array())
+    {
+        try{
+            $context = array_merge($this->defaultContext, $context);
+            return parent::addRecord($level, $message, $context);
+
+        } catch (\Exception $e){
+            trigger_error($e, E_USER_WARNING);
+            return false;
+        }
+    }
+
+    /**
+     * Add one default context values
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return $this
+     */
+    public function addDefaultContextValue($key, $value)
+    {
+        $this->defaultContext[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Adds default context values
+     *
+     * @param array $defaults
+     *
+     * @return $this
+     */
+    public function addDefaultContext(array $defaults)
+    {
+        $this->defaultContext = array_merge($this->defaultContext, $defaults);
+
+        return $this;
+    }
+}
